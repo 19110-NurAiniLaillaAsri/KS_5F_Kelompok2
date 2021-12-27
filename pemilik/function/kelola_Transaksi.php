845e03f2c00be588a96d1821c99b0b39a6cf8320
19159-AlysaSalsabila <83756242+19159-AlysaSalsabila@users.noreply.github.com>
2021-12-27 19:37:58 +0700

    Update kelola_Transaksi.php

--- pemilik/function/kelola_Transaksi.php
+++ pemilik/function/kelola_Transaksi.php
@@ -4,3 +4,7 @@
 //Read Data Motor//
     $sqlRead = "SELECT * FROM transaksi ORDER BY status_transaksi = 'Waiting' DESC";
     $queryRead = mysqli_query($koneksi, $sqlRead);
+//Create Transaksi
+    if(isset($_POST["tambahTransaksi"])){
+        $id_motor = $_POST["id_motor"];
+        $id_user = $_POST["id_user"];
