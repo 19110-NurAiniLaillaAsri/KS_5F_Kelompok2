705d98a4f670ebdf31d986992ab72ed6f41b38b4
19114-NurliAlifah <83756741+19114-NurliAlifah@users.noreply.github.com>
2021-12-27 11:30:20 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,3 @@
 <?php
-    $koneksi = mysqli_connect("localhost", "root", "","ks_5f_kelompok2") or die("Koneksi Gagal");
+    $koneksi = mysqli_connect("localhost", "root", "","ks_5f_kelompok2") or die("Database Tidak Terkoneksi");
 ?>
