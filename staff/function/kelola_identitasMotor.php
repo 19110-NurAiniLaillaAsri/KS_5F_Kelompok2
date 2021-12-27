d38877013e238d4d2da65e940cfeaec2f7711b4e
19114-NurliAlifah <83756741+19114-NurliAlifah@users.noreply.github.com>
2021-12-27 11:44:21 +0700

    Update kelola_identitasMotor.php

--- staff/function/kelola_identitasMotor.php
+++ staff/function/kelola_identitasMotor.php
@@ -2,6 +2,7 @@
     require 'session.php';
     require '../koneksi.php';
 
-    //Read Data Motor//
-
+//Read Data Motor//
+    $sqlRead = "SELECT * FROM data_motor";
+    $queryRead = mysql_query($koneksi, $sqlRead);
 ?>
