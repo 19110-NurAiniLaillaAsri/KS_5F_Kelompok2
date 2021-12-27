131930aed28c8eabfef4426dd7be1a64545a587c
19191-HafizhHasanAR <83756592+19191-HafizhHasanAR@users.noreply.github.com>
2021-12-27 23:35:11 +0700

    Update M_identitasMotor.php
    tombol konfirmasi edit

--- staff/M_identitasMotor.php
+++ staff/M_identitasMotor.php
@@ -279,4 +279,22 @@
                                                         </div>
                                                     </div>
                                                 </div>
+    <!-- Tombol Konfirmasi Edit-->
+                                                <div class="modal fade" id="alertEditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-centered">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLabel">Edit Data</h5>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                Edit data ID Motor '.$id_motor[$i].'?
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                                                                <button type="submit" class="btn btn-primary" name="updateMotor">Yes</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div> 
                                                 
