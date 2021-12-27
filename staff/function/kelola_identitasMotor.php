30d002939c29319b2ea2bd453084b87536997558
19114-NurliAlifah <83756741+19114-NurliAlifah@users.noreply.github.com>
2021-12-27 11:53:27 +0700

    Update kelola_identitasMotor.php

--- staff/function/kelola_identitasMotor.php
+++ staff/function/kelola_identitasMotor.php
@@ -7,4 +7,15 @@
     $queryRead = mysqli_query($koneksi, $sqlRead);
 
 //Create Data Motor
+    if(isset($_POST["tambahMotor"])) {
+        $nama_pemilik = $_POST["nama_pemilik"];
+        $plat_no = $_POST["plat_no"];
+        $type = $_POST["type"]
+        $warna = $_POST["Warna];
+        $tahun_pembuatan = $_POST["tahun_pembuatan"];
+        $masa_berlaku_stnk = $_POST["masa_berlaku_STNK];
+        $pajak = $_POST["pajak'];
+        $harga_asli = $_POST["harga_asli"];
+        $harga_jual = $_POST["harga_jual"];
+        $odometer = $_POST["odometer"];
 ?>
