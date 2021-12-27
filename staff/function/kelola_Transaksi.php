d8ac76f8d425656366b6854e30935910e5135986
19159-AlysaSalsabila <83756242+19159-AlysaSalsabila@users.noreply.github.com>
2021-12-27 19:46:30 +0700

    Update kelola_Transaksi.php

--- staff/function/kelola_Transaksi.php
+++ staff/function/kelola_Transaksi.php
@@ -8,3 +8,11 @@
     if(isset($_POST["tambahTransaksi"])){
         $id_motor = $_POST["id_motor"];
         $id_user = $_POST["id_user"];
+        // Random ID Transaksi
+        $queryID = mysqli_query($koneksi, "SELECT max(id_transaksi) as id_terbesar FROM transaksi");
+        $data = mysqli_fetch_array($queryID);
+        $id_baru = $data['id_terbesar'];
+        $urutan = (int) substr($id_baru, 3, 4);
+        $urutan++;
+        $huruf = "TRN";
+        $id_transaksi = $huruf . sprintf("%04s", $urutan);
