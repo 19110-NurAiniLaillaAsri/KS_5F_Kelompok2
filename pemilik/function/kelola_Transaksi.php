4215a92985db8cf3f395d225535c0f48a41d4591
19159-AlysaSalsabila <83756242+19159-AlysaSalsabila@users.noreply.github.com>
2021-12-27 19:39:31 +0700

    Update kelola_Transaksi.php

--- pemilik/function/kelola_Transaksi.php
+++ pemilik/function/kelola_Transaksi.php
@@ -16,3 +16,35 @@
         $urutan++;
         $huruf = "TRN";
         $id_transaksi = $huruf . sprintf("%04s", $urutan);
+        // Upload Bukti Transfer
+        $bukti = $_FILES['bukti_transfer']['name'];
+        $x = explode('.',$bukti);
+        $eks = strtolower(end($x));
+        $asal = $_FILES['bukti_transfer']['tmp_name'];
+        $dir = "../buktiTransfer/";
+        $foto = uniqid();
+        $foto .= '.'.$eks;
+        $targetFile = $dir.$foto;
+        $uploadOk = 1;
+        if (file_exists($targetFile)){
+            $uploadOk = 0;
+        }
+        if ($uploadOk == 0){
+            echo '<script>alert("Nama file sudah ada!");</script>';
+        } else if ($bukti=="") {
+            echo "<script>alert('harap upload bukti transaksi!')
+            window.location.replace('M_transaksi.php');</script>";
+        } else {
+            if (move_uploaded_file($asal, $targetFile)){
+                $queryDataMotor = mysqli_query($koneksi, "UPDATE data_motor SET status = 'Booked' WHERE id_motor = '$id_motor'");
+                $queryCreate = "INSERT INTO transaksi VALUES ('$id_transaksi','$id_motor','$id_user',NOW(),'$foto','Waiting')";
+                $createData = mysqli_query($koneksi, $queryCreate);
+                if ($createData){
+                    echo "<script>alert('Motor Booked, Transaksi berhasil ditambahkan!')
+                    window.location.replace('M_transaksi.php');</script>";
+                }
+            } else {
+                echo '<script>alert("Proses Upload GAGAL!");</script>';
+            }
+        }
+    }
