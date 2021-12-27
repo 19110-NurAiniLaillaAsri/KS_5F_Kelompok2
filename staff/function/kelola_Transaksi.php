93cbea5558e09e840165f50bd37580b3990604df
19159-AlysaSalsabila <83756242+19159-AlysaSalsabila@users.noreply.github.com>
2021-12-27 19:45:31 +0700

    Update kelola_Transaksi.php

--- staff/function/kelola_Transaksi.php
+++ staff/function/kelola_Transaksi.php
@@ -1,3 +1,6 @@
 <?php
     require 'session.php';
     require '../koneksi.php';
+//Read Data Motor//
+    $sqlRead = "SELECT * FROM transaksi ORDER BY status_transaksi = 'Waiting' DESC";
+    $queryRead = mysqli_query($koneksi, $sqlRead);
