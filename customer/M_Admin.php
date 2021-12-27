a99d1f58e00fb5ab02e7b921ca67c27ebf138666
19197-KarolinaEkaPurnama <1910631170197@student.unsika.ac.id>
2021-12-27 13:22:41 +0700

    Update M_Admin.php

--- customer/M_Admin.php
+++ customer/M_Admin.php
@@ -15,3 +15,13 @@
     <div class="d-flex" id="wrapper">
         <div class="bg-3" id="sidebar-wrapper">
             <div class="sidebar-heading text-center py-4 warna-1 fs-4 fw-bold text-uppercase">
+                <i class="me-2"></i>Penjualan<br>Motor Bekas
+            </div>
+            <div class="list-group list-group-flush my-3">
+                <a href="index.php" class="list-group-item list-group-item-action bg-transparent fw-bold warna-1"><i class="fas fa-home me-2"></i>Home</a>
+                <a href="M_transaksi.php" class="list-group-item list-group-item-action bg-transparent warna-1 fw-bold"><i class="fas fa-tags me-2"></i>Transaksi</a>
+                <a href="M_Admin.php" class="list-group-item list-group-item-action bg-transparent active-bar fw-bold"><i class="fas fa-users me-2"></i>Kontak Admin</a>
+            </div>
+        </div>
+        <div id="page-content-wrapper">
+            <nav class="navbar navbar-expand-lg navbar-light bg-2 py-4 px-4">
