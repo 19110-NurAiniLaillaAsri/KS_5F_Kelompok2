65b99cff4fa080d5bc97ad6a5bd79db7b9f2b86f
19110-NurAiniLaillaAsri <83807811+19110-NurAiniLaillaAsri@users.noreply.github.com>
2021-12-29 13:36:14 +0700

    Update Modal_Transaksi.php

--- pemilik/Modal_Transaksi.php
+++ pemilik/Modal_Transaksi.php
@@ -51,3 +51,44 @@
             </div>
         </div>
     </div>  
+<!-- Modal Proses Transaksi -->
+    <form method="post">
+        <div class="modal fade" id="proses<?php echo $row['id_transaksi'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Proses Transaksi</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="invisible position-absolute">
+                            <input type="text" class="form-control" name="id_transaksi" value="<?php echo $row['id_transaksi'] ?>" readonly>
+                        </div>
+                        <div class="text-center pb-3">
+                            <img src="../buktiTransfer/<?php echo $row['bukti_transfer'] ?>" style="height: 150px;">
+                        </div>
+                        <div class="row">
+                            <div class="col-5 mt-1"><label>Id Motor</label></div>
+                            <div class=col>
+                                <input class="form-control" name="id_motor" type="text" value="<?php echo $row['id_motor'] ?>" readonly><br>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-5 mt-1"><label>Id User</label></div>
+                            <div class=col>
+                                <input class="form-control" name="id_user" type="text" value="<?php echo $row['id_user'] ?>" readonly><br>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-5 mt-1"><label>Tanggal Transaksi</label></div>
+                            <div class=col>
+                                <input class="form-control" name="tgl_transaksi" type="date" value="<?php echo $row['tgl_transaksi'] ?>" readonly><br>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
