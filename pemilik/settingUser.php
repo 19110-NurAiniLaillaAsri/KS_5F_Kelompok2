b5bf42bc771fce2d276d738472468d8024ed4e87
19194-IndahFauziah <84067766+19194-IndahFauziah@users.noreply.github.com>
2022-01-02 20:35:46 +0700

    Update settingUser.php

--- pemilik/settingUser.php
+++ pemilik/settingUser.php
@@ -49,14 +49,13 @@
             }
         }
     }
-echo '
+    echo '
         <form method="POST">
             <div class="modal fade" id="edit'.$id_user.'" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title"
-id="exampleModalLabel">Edit Data User</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Edit Data User</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
