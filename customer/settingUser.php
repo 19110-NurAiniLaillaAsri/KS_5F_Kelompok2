a2271edf936f38d849b25a2ae4659b62673bb265
19195-IvanRizwan <84071956+19195-IvanRizwan@users.noreply.github.com>
2022-01-02 19:29:03 +0700

    Update settingUser.php

--- customer/settingUser.php
+++ customer/settingUser.php
@@ -2,7 +2,7 @@
     $sqlEditUser = "SELECT * FROM user WHERE id_user = '$id_user'";
     $querySetting = mysqli_query($koneksi, $sqlEditUser);
     $rowSet = mysqli_fetch_array($querySetting);
-    // Setting User
+    // Setting User.
     if(isset($_POST["eksekusiEdit"])){
         $id_user = $_POST["id_user"];
         $password_lama = $_POST["password_lama"];
