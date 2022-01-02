53380540a67afa638be035cc93d57863f63c29c2
19195-IvanRizwan <84071956+19195-IvanRizwan@users.noreply.github.com>
2022-01-02 19:27:27 +0700

    Update session.php

--- staff/function/session.php
+++ staff/function/session.php
@@ -1,7 +1,7 @@
 <?php
     require '../koneksi.php';
     require '../auth.php';
-// Ambil Session
+// Ambil Session.
     session_start();
     $id_user = $_SESSION['id_user'];
     $querySession = mysqli_query($koneksi, "SELECT * FROM user WHERE id_user='$id_user'");
